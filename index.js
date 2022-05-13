5922a34505e1bbb4eed204d9265b8d5db27e3f8a
shabi m f <shabi@adagency.design>
2022-05-13 14:51:15 +0530

    updated

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log("testing....")
+console.log("Creating....")
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const converter = require('json-2-csv');
+
+const url = "https://cbic-gst.gov.in/gst-goods-services-rates.html";
+
+
+async function scrapGST()
+{
+try{
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  //console.log($.html());
+const report = $("body table tbody tr");
+//console.log(report.html());
+
+const gstdatas = [];
+
+report.each(
+  (i,el) => {
+//tds = td.text();
+//gstdatas.push($(tr).text());
+//const code = $(el).find("td:eq(2)").text();
+/*
+if(code != "")
+{
+  */
+const getData = {schedules:"",sno:"",code:"",description:"",cgst:"",sgst:"",igst:""};
+getData.schedules =$(el).find("td:eq(0)").text();
+getData.sno =$(el).find("td:eq(1)").text();
+getData.code =$(el).find("td:eq(2)").text();
+getData.description = $(el).find("td:eq(3)").text();
+getData.cgst =$(el).find("td:eq(4)").text();
+getData.sgst = $(el).find("td:eq(5)").text();
+getData.igst =$(el).find("td:eq(6)").text();
+/*
+}
+*/
+gstdatas.push(getData);
+
+
+});
+
+
+  console.log(gstdatas);
+
+  converter.json2csv(gstdatas, (err, csv) => {
+      if (err) {
+          throw err;
+      }
+
+      // print CSV string
+      //console.log(csv);
+      fs.writeFileSync("gstdatasfinal.csv",csv);
+  });
+
+ console.log("--------------------------------Completed creation-------------------");
+}
+catch(err){
+console.error(err);
+}
+  }
+
+scrapGST();
